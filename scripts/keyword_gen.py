8b286cb53ba3f65f7ff43af55d70f3e713030e96
Christophe <lauch111@gmail.com>
2025-06-11 18:48:55 +0200

    README update

--- scripts/keyword_gen.py
+++ scripts/keyword_gen.py
@@ -3,6 +3,7 @@ import yaml
 import os
 from pytools.outils import collect_md_head_data
 import re
+import json
 
 
 readme_header = '''---
@@ -13,12 +14,14 @@ permalink: /
 '''
 
 
-def collect_all_keywords(fnames: list):
+def collect_keywords_n_categories(fnames: list):
     out = dict()
     for _fn in fnames:
         head = collect_md_head_data(_fn)
         if 'keywords' not in head:
             head['keywords'] = []
+        if 'category' not in head:
+            head['category'] = []
         head['fname'] = os.path.split(_fn)[1].split('.')[0]
         out[_fn] = head
     return out
@@ -28,7 +31,7 @@ def assign_fnames_to_keywords(collected_kw_data: dict):
     out = dict()
     date_pat = re.compile(r'^\[(\d\d\d\d)-(\d\d)-(\d\d) ')
     for _k in collected_kw_data.keys():
-        lnk = f'[{str(collected_kw_data[_k]["date"])} {collected_kw_data[_k]["title"]}]'
+        lnk = f'[{collected_kw_data[_k]["title"]}]'
         lnk += '('
         lnk += '{{site.baseurl}}'
         lnk += f'/{collected_kw_data[_k]["category"]}'
@@ -43,13 +46,37 @@ def assign_fnames_to_keywords(collected_kw_data: dict):
     for _k in out.keys():
         out[_k] = sorted(
             out[_k],
-            key=lambda x: (re.search(date_pat, x).group(1), re.search(date_pat, x).group(2), re.search(date_pat, x).group(3)),
+            #key=lambda x: (re.search(date_pat, x).group(1), re.search(date_pat, x).group(2), re.search(date_pat, x).group(3)),
+            #reverse=True
+        )
+    return out
+
+
+def assign_fnames_to_categories(collected_data: dict):
+    out = dict()
+    # date_pat = re.compile(r'^\[(\d\d\d\d)-(\d\d)-(\d\d) ')
+    for _k in collected_data.keys():
+        lnk = f'[{collected_data[_k]["title"]}]'
+        lnk += '('
+        lnk += '{{site.baseurl}}'
+        lnk += f'/{collected_data[_k]["category"]}'
+        lnk += f'/{collected_data[_k]["fname"]}.html'
+        lnk += ')'
+        if collected_data[_k]['category'] not in out:
+            out[collected_data[_k]['category']] = []
+        out[collected_data[_k]['category']].append(lnk)
+
+    # sorting
+    for _k in out.keys():
+        out[_k] = sorted(
+            out[_k],
+            #key=lambda x: (re.search(date_pat, x).group(1), re.search(date_pat, x).group(2), re.search(date_pat, x).group(3)),
             reverse=True
         )
     return out
 
 
-def complete_readme(readme_head: str, keywords: dict):
+def complete_readme_keywords(readme_head: str, keywords: dict):
     out = readme_head + '\n'
     out += '## Keywords\n\n'
     out += '; '.join(
@@ -64,13 +91,30 @@ def complete_readme(readme_head: str, keywords: dict):
     return out
 
 
+def complete_readme_categories(readme_head: str, _categories: dict):
+    out = readme_head + '\n'
+    out += '## Keywords\n\n'
+    out += '; '.join(
+        f'[{_item}]('+'{{site.baseurl}}'+f'/#{_item.lower().replace(" ", "-")})' for _item in sorted(_categories)
+        # f'[{_item}]' for _item in sorted(keywords)
+    )
+    out += '\n\n'
+    for _k in sorted(_categories):
+        out += f'### {_k}\n\n'
+        out += '\n'.join(f'- {_item}' for _item in _categories[_k])
+        out += '\n\n'
+    return out
+
+
 def run(jekyll_loc: str):
     with open(os.path.join(jekyll_loc, '_config.yml'), 'r') as _f:
         config_data = yaml.safe_load(_f)
     all_posts = [os.path.join(jekyll_loc, f'_posts/{_fn}') for _fn in os.listdir(os.path.join(jekyll_loc, '_posts'))]
-    keywords = collect_all_keywords(all_posts)
-    keywords = assign_fnames_to_keywords(keywords)
-    readme = complete_readme(readme_header, keywords)
+    keywords_n_categories = collect_keywords_n_categories(all_posts)
+    keywords = assign_fnames_to_keywords(keywords_n_categories)
+    categories = assign_fnames_to_categories(keywords_n_categories)
+    #readme = complete_readme_keywords(readme_header, keywords)
+    readme = complete_readme_categories(readme_header, categories)
     # write README.md file
     readme_loc = os.path.join(jekyll_loc, 'README.md')
     with open(readme_loc, 'w') as _f:
